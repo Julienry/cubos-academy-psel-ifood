f1e89563f791382773d3f7c756f69d7a24b0d0a1
Julienry <julienry.andrade@gmail.com>
2021-08-10 19:55:25 -0300

    Quarto exercício

---
+++ Esconde-esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+	//seu codigo aqui
+    if(numero > 20){
+        for(var i = 21; i<=numero; i++){
+            console.log(i);
+        }
+    }
+    else{
+        console.log("VOCE CONSEGUE, MANINHO");
+    }
+    
+}
